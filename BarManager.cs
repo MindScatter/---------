4f3b2bd12815dba3876c04c118829f22ee0befc6
MindScatter <Outburst91@yahoo.com>
2019-07-22 01:18:30 -0500

    Add files via upload

---
+++ BarManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class BarManager
+{
+    [SerializeField]
+    private BarScript bar;
+
+    [SerializeField]
+    private float maxVal;
+
+    [SerializeField]
+    private float currentVal;
+
+    [SerializeField]
+    private float minVal = 0.0f;
+
+    public float CurrentVal
+    {
+        get
+        {
+            return currentVal;
+        }
+        set
+        {
+            this.currentVal = value;
+            bar.Value = currentVal;
+        }
+    }
+
+    public float MaxVal
+    {
+        get
+        {
+            return maxVal;
+        }
+        set
+        {
+            this.maxVal = value;
+            bar.MaxValue = maxVal;
+        }
+    }
+
+    public void Update()
+    {
+//        this.MaxVal = maxVal;
+ //       this.CurrentVal = currentVal;
+        if(currentVal <= 100)
+        {
+            currentVal -= Time.deltaTime;
+        }
+
+        if(currentVal >= 100)
+        {
+            currentVal = maxVal;
+        }
+
+        if(currentVal <= 0)
+        {
+            currentVal = minVal;
+        }
+    }
+}
---
+++ BarScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarScript : MonoBehaviour
+{
+    [SerializeField]
+    private float fillAmount;
+
+    [SerializeField]
+    private Image content;
+
+    public float MaxValue { get; set; }
+
+    public float Value
+    {
+        set
+        {
+            fillAmount = Map(value, 0, MaxValue, 0, 1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleBar();
+    }
+
+    private void HandleBar()
+    {
+        if (fillAmount != content.fillAmount)
+        { 
+            content.fillAmount = fillAmount;
+        }
+    }
+
+    private float Map(float value, float inMin, float inMax, float outMin, float outMax)
+    {
+        return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+        //(78 - 0) * (1 - 0) / (230 - 0) + 0
+        //78 * 1 / 230 = 0,339
+    }
+}
