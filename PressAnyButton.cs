d206f1bda72ba8b3b19369aa8f6eab1369f969a8
MindScatter <Outburst91@yahoo.com>
2020-11-01 01:44:59 -0500

    Add files via upload

---
+++ PressAnyButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressAnyButton : MonoBehaviour
+{
+    public AudioClip sfxButton;
+
+    private bool oneshotSfx;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            if(!oneshotSfx)
+            {
+ //               AudioSource.PlayClipAtPoint(sfxButton, Vector3.zero);
+                Invoke("LoadScene", 0.5f);
+                oneshotSfx = true;
+            }
+        }
+    }
+
+    void LoadScene()
+    {
+        Debug.Log("new scene");
+        //load gameplay scene
+//        Application.LoadLevel("");
+    }
+}
